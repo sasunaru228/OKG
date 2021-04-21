b2415aa4003152cd90d4c2f4acab9c0fbe391d52
sasunaru228 <od26102000@gmail.com>
2021-04-21 10:34:34 +0300

    add popup section8

--- js/script.js
+++ js/script.js
@@ -6,8 +6,6 @@ new Splide( '.splide', {
 } ).mount();
 
 
-
-
 let modal1 = new tingle.modal({
     footer: false,
     stickyFooter: false,
@@ -32,3 +30,71 @@ document.querySelector(".modal2").addEventListener('click', () => {
 
 modal1.setContent('<img class="modal-IMG" src="img/lohman.png" alt="content">')
 modal2.setContent('<img class="modal-IMG" src="img/tubex.png" alt="content">')
+
+
+
+
+
+// modal 8.1
+
+let modal8_1 = new tingle.modal({
+
+})
+
+document.querySelector("#modal8_1_link").addEventListener('click', () => {
+    modal8_1.open()
+})
+document.querySelector("#modal8_1_close").addEventListener('click', () => {
+    modal8_1.close()
+})
+
+let modal8_1_content = document.querySelector(' #modal8_1_content');
+modal8_1.setContent(modal8_1_content);
+
+// modal 8.2
+
+let modal8_2 = new tingle.modal({
+
+})
+
+document.querySelector("#modal8_2_link").addEventListener('click', () => {
+    modal8_2.open()
+})
+document.querySelector("#modal8_2_close").addEventListener('click', () => {
+    modal8_2.close()
+})
+
+let modal8_2_content = document.querySelector(' #modal8_2_content');
+modal8_2.setContent(modal8_2_content);
+
+// modal 8.3
+
+let modal8_3 = new tingle.modal({
+
+})
+
+document.querySelector("#modal8_3_link").addEventListener('click', () => {
+    modal8_3.open()
+})
+document.querySelector("#modal8_3_close").addEventListener('click', () => {
+    modal8_3.close()
+})
+
+let modal8_3_content = document.querySelector(' #modal8_3_content');
+modal8_3.setContent(modal8_3_content);
+
+// modal 8.4
+
+let modal8_4 = new tingle.modal({
+
+})
+
+document.querySelector("#modal8_4_link").addEventListener('click', () => {
+    modal8_4.open()
+})
+document.querySelector("#modal8_4_close").addEventListener('click', () => {
+    modal8_4.close()
+})
+
+let modal8_4_content = document.querySelector(' #modal8_4_content');
+modal8_4.setContent(modal8_4_content);
